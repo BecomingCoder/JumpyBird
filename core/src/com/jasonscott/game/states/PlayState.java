f347304c92f190103b45893fad6f086cfcdddff2
Jason Scott <becomingcoder78@gmail.com>
2016-05-26 23:23:41 -0400

    Added flying ability to bird sprite

--- core/src/com/jasonscott/game/states/PlayState.java
+++ core/src/com/jasonscott/game/states/PlayState.java
@@ -1,5 +1,6 @@
 package com.jasonscott.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jasonscott.game.JumpyDemo;
@@ -10,16 +11,20 @@ import com.jasonscott.game.sprites.Bird;
  */
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
 
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, JumpyDemo.WIDTH / 2, JumpyDemo.HEIGHT / 2);
+        bg = new Texture("bg.png");
     }
 
     @Override
     protected void handleInput() {
+        if(Gdx.input.justTouched())
+            bird.jump();
 
     }
 
@@ -34,6 +39,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
 
