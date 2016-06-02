38f70786cb93c27f8d9e95daa5562e2fb94e8073
Jason Scott <becomingcoder78@gmail.com>
2016-06-01 22:51:55 -0400

    Added music and sound effects

--- core/src/com/jasonscott/game/JumpyDemo.java
+++ core/src/com/jasonscott/game/JumpyDemo.java
@@ -3,6 +3,7 @@ package com.jasonscott.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jasonscott.game.states.GameStateManager;
@@ -16,11 +17,17 @@ public class JumpyDemo extends ApplicationAdapter {
     private GameStateManager gsm;
     private SpriteBatch batch;
 
+    private Music music;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
         gsm = new GameStateManager();
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.1f);
+        music.play();
         Gdx.gl.glClearColor(1, 0, 0, 1);
         gsm.push(new MenuState(gsm));
 
@@ -32,4 +39,10 @@ public class JumpyDemo extends ApplicationAdapter {
         gsm.update(Gdx.graphics.getDeltaTime());
         gsm.render(batch);
 	}
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        music.dispose();
+    }
 }
